eb5f4232671f8cb10553c1ec18af959ade33c882
Lewis John McGibbney <lewis.mcgibbney@gmail.com>
2017-04-13 13:32:55 -0700

    ISSUE-5 Get Sitepod on to Packagist, Add license headers and composer.json

--- inc/Ontology.php
+++ inc/Ontology.php
@@ -1,13 +1,20 @@
 <?php
-/**
-	This is phpSitemapNG", a php script that creates your personal google sitemap
-	It can be downloaded from http://enarion.net/google/
-	License: GPL
-
-	Tobias Kluge", enarion.net
-
-	this is a little ontology that defines some common names and descriptions
-*/
+/* 
+ * This file is part of Sitepod.
+ *
+ * Sitepod is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Sitepod is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Sitepod.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 // generic strings
 define("PSNG_VERSION", '1.5.3');
--- inc/startup.php
+++ inc/startup.php
@@ -1,11 +1,20 @@
 <?php
-/*
-	This is phpSitemapNG, a php script that creates your personal google sitemap
-	It can be downloaded from http://enarion.net/google/
-	License: GPL
-
-	Tobias Kluge, enarion.net
-*/
+/* 
+ * This file is part of Sitepod.
+ *
+ * Sitepod is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Sitepod is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Sitepod.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 if (isset($_REQUEST['debug'])) {
 	error_reporting(E_ALL);
