9274217d76746c24bc522eaaddef0bdd9bf6b871
sigee <sigee15@gmail.com>
2017-05-25 14:59:14 +0200

    Add a check to the GeoIp::getGeoData method.

--- inc/classes/GeoIp.php
+++ inc/classes/GeoIp.php
@@ -4,10 +4,19 @@ class GeoIp
 {
     private static $SERVER_URL = 'http://freegeoip.net/json/';
 
+    /**
+     * Returns the geo data for the given IP address.
+     *
+     * @param string $ipAddress The IP address to get geo data for.
+     * @return array
+     */
     public function getGeoData($ipAddress)
     {
         $json = file_get_contents(self::$SERVER_URL . $ipAddress);
-        /** @TODO: Add some checks to be sure we got the right response. */
-        return json_decode($json, true);
+        if ($json) {
+            /** @TODO: Add some more checks to be sure we got the right response. */
+            return json_decode($json, true);
+        }
+        return [];
     }
 }
