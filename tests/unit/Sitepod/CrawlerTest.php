02676c0f4e544de3e7be090a7d8e754d019f105c
Dávid Szigecsán <sigee15@gmail.com>
2017-06-10 18:56:46 +0200

    ISSUE-11 - Add travis-ci build to Sitepod (#22)
    * Add .travis.yml
    
    * Add a demo test
    
    * Add a phpunit config xml
    
    * Update composer.json

---
+++ tests/unit/Sitepod/CrawlerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Sitepod;
+
+use PHPUnit\Framework\TestCase;
+
+class CrawlerTest extends TestCase
+{
+    function testNothing()
+    {
+        $this->assertTrue(true);
+    }
+}
