0c82cced03efb725d50da2db9ee5199856d69125
Lewis John McGibbney <lewis.mcgibbney@gmail.com>
2017-06-19 15:40:04 -0700

    ISSUE-27 Fix licensing headers

--- templates/edit_files.php
+++ templates/edit_files.php
@@ -1,6 +1,5 @@
 <?php
-/* 
- * This file is part of Sitepod.
+/* This file is part of Sitepod.
  *
  * Sitepod is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,7 +14,6 @@
  * You should have received a copy of the GNU General Public License
  * along with Sitepod.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 	debug($FILE, "templates/edit_files: will display following files");
 	$layout = '
 	<form action="'.PSNG_ACTION_SETTINGS_WRITESITEMAP_USERINPUT.'" method="post" accept-charset=utf-8>
